a1cdccb4452bbf919a8d6c493072da60c9b8010b
giild <peter@giild.com>
2022-03-11 08:36:39 -0500

    initial checkin

---
+++ src/Animation.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Canvas from './Canvas'
+
+function Animation() {
+
+  return (
+      <>
+        <Canvas/>
+      </>
+  )
+}
+
+export default Animation
---
+++ src/Canvas.js
@@ -0,0 +1,34 @@
+import React, { useRef, useEffect } from 'react'
+
+const Canvas = props => {
+    const canvasRef = useRef(null)
+
+    const draw = (ctx, frameCount) => {
+        ctx.fillStyle = '#000000'
+        ctx.beginPath()
+        // context.arc(x, y, radius, startAngle, endAngle, counterClockwide)
+        ctx.arc(50, 100, 45 * Math.sin(frameCount*0.01)**2, 0, 2*Math.PI)
+        ctx.fill()
+    }
+
+    useEffect(() => {
+        const canvas = canvasRef.current
+        const context = canvas.getContext('2d')
+        let frameCount = 0
+        let animationFrameId
+
+        const render = () => {
+            frameCount++
+            draw(context, frameCount)
+            animationFrameId = window.requestAnimationFrame(render)
+        }
+        render()
+    
+        return() => {
+            window.cancelAnimationFrame(animationFrameId)
+        }
+        }, [draw])
+    return <canvas ref={canvasRef} {...props} />
+}
+
+export default Canvas
